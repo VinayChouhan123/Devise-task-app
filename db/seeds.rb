d27dbf526a67c8d212c65676c5d2cab603e9af0b
VinayChouhan123 <vinayyuvasoft375@gmail.com>
2022-05-10 14:50:06 +0530

    add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.new(
+    email: "vinayrajput@gmail.com",
+    username: "vinayrajput",
+    mobile: 4569854789,
+    password: 123456,
+    password_confirmation: 123456
+)
+user.add_role :admin
